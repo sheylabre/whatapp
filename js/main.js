308378ea8f9d9e8dbc5833418d666d1461e65db5
sheylabre <sheylabrenaicha@hotmail.com>
2016-10-10 13:09:39 -0500

    version 0.1

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function escribir() {
+    document.getElementById('mensajes').addEventListener('keypress', function(e) {
+        if (e.keyCode == 13) {
+            document.write(document.getElementById("mensajes");
+        }
+    })
+}
+
+document.getElementById('mensajes').addEventListener('keypress', function(e) {
+    // si la tecla presionada es enter// si la tecla presionada es enter
+    if (e.keyCode == 13) {
+        //el codigo que quiero ejecutar
+    }
+})
