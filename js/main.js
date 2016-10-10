f0b433301dc382ec851ea918d303efcaf944a4cc
sheylabre <sheylabrenaicha@hotmail.com>
2016-10-10 13:55:11 -0500

    version1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
-function escribir() {
-    document.getElementById('mensajes').addEventListener('keypress', function(e) {
-        if (e.keyCode == 13) {
-            document.write(document.getElementById("mensajes");
-        }
-    })
-}
+window.addEventListener("load",function(){
+    var mensaje = document.getElementById('mensajes');
+    mensajes.addEventListener('keypress', function(e) {
+        var tecla = e.keyCode;
+        if ( tecla == 13) {
+            var conversacion = document.getElementById("conversacion");
+            var contenedor = document.createElement("div");
+                contenedor.classList.add("w-message", "w-message-out");
+                conversacion.appendChild(contenedor);
+            var segundContenedor = document.createElement("div");
+                segundContenedor.classList.add("w-message-text");
+                contenedor.appendChild(segundContenedor);
+            var parrafo = document.createElement("p");
 
-document.getElementById('mensajes').addEventListener('keypress', function(e) {
-    // si la tecla presionada es enter// si la tecla presionada es enter
-    if (e.keyCode == 13) {
-        //el codigo que quiero ejecutar
-    }
-})
+
+        }
+    });
+});
+
+
